4ae0d8fbafbcd624950a32fbffac0a25ba4697e0
laitdsgn <tomek08.konto@gmail.com>
2025-06-20 21:24:40 +0200

    tesT

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Offer> offers = new();
+offers.Add(new Offer("dadsa", 2, "dasdasasd")); // <- przed app.Run()
+
+app.MapGet("/offers", () => Results.Json(offers));
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+record Offer(string Title, decimal Price, string Link);
